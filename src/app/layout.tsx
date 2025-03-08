4ff9e607551259a6bc82f2d954dd765943d1275c
Fernando Nunez <fernnunez.iii@gmail.com>
2025-03-07 20:33:32 -0800

    Bug Fix with Go Back in Browser

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
 import Navbar from './components/navbar/navbar';
-import Footer from './components/footer/footer'
+import Footer from './components/footer/footer';
+import ScrollToTop from './components/scrollBack/scrollBack';
 import "./globals.css";
 
 const poppins = Poppins({
@@ -22,21 +21,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-
-  useEffect(() => {
-    // Control browser's scroll restoration behavior
-    if ('scrollRestoration' in window.history) {
-      window.history.scrollRestoration = 'manual';
-    }
-    
-    // Reset scroll position when route changes
-    window.scrollTo(0, 0);
-  }, [pathname]); // This will run whenever the path changes
-
   return (
     <html lang="en">
       <body className={`${poppins.variable} antialiased`}>
+        <ScrollToTop />
         <Navbar/>
         {children}
         <Footer/>
